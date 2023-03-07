3c8743973cead89d03ad8cbb8d6e282750418465
lpetzinger <lucas.petzinger@mercadolivre.com>
2023-03-07 09:32:58 -0300

    add login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import Header from "../components/Header";
+
+function Login() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (username === "eliane" && password === "Petz1973") {
+      navigate("/criar");
+    } else {
+      alert("Usuário ou senha incorretos!");
+    }
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <h1>Login</h1>
+      <div className="form">
+        <label htmlFor="username">Usuário:</label>
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          id="username"
+          name="username"
+          type="text"
+        />
+
+        <label htmlFor="password">Senha:</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          id="password"
+          name="password"
+          type="password"
+        />
+
+        <button onClick={handleLogin} className="button" type="submit">
+          Entrar
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Chat from "../Chat";
 import GenerateRaffle from "./GenerateRaffle";
+import Login from "./Login";
 
 import RafflePage from "./RafflePage";
 
@@ -10,7 +11,7 @@ const Path = () => {
     <BrowserRouter>
       <Routes>
         <Route element={<RafflePage />} path="/rifa" exact />
-        {/* <Route element={<Chat />} path="/" exact /> */}
+        <Route element={<Login />} path="/" exact />
         <Route element={<GenerateRaffle />} path="/criar" exact />
       </Routes>
     </BrowserRouter>
